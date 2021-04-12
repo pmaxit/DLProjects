65e3b72bc020d0d1f9226990666e80dc4e5f201f
puneet girdhar <puneet@puneets-mbp.lan>
2021-04-12 14:58:00 -0500

    create running file

--- runs/data_utils.py
+++ runs/data_utils.py
@@ -17,12 +17,16 @@ import numpy as np
 import pandas as pd
 import spacy
 import random
-from torch.utils.data import DataLoader
-from torch.nn.utils.rnn import pad_sequence
-from torch.utils.data import DataLoader
-import subprocess
-from functools import partial
+import string
 
+       
+from torchtext.experimental.datasets.translation import *
+from torchtext.data.datasets_utils import _wrap_datasets
+from torch.nn.utils.rnn import pad_sequence
+import pytorch_lightning as pl
+
+
+PAD_IDX = 1
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
@@ -107,7 +111,53 @@ class MyDataModule(pl.LightningDataModule):
         return DataLoader(self.valid_data, batch_size = self.batch_size, shuffle=False, 
                 collate_fn= self.generate_batch)
 
+
+def random_examples(n_examples, n_largest):
+    letters = string.ascii_lowercase
+    for _ in range(n_examples):
+        l = random.choice(range(1,n_largest+1))
+        x = ''.join(random.choice(letters) for _ in range(l))
+        y = ':'+ x[::-1]
+        yield x,y
+ 
+def generate_batch_new(data_batch):
+    src_batch, trg_batch = [], []
+    for (src, trg) in data_batch:
+        src_batch.append(src)
+        trg_batch.append(trg)
     
+    src_batch = pad_sequence(src_batch, padding_value = PAD_IDX)
+    trg_batch = pad_sequence(trg_batch, padding_value = PAD_IDX)
+    
+    # get mask for them as well
+    return src_batch, trg_batch
+
+def ReversedString(data, tokenizer, split_=('train','test')):
+    # split the data into three parts
+    src_tokenizer, trg_tokenizer = tokenizer
+    src_text_vocab_transform = sequential_transforms(src_tokenizer)
+    trg_text_vocab_transform = sequential_transforms(trg_tokenizer)
+    
+    # build vocab only on training dataset
+    src_vocab = build_vocab(data['train'], src_text_vocab_transform, index=0)
+    trg_vocab = build_vocab(data['train'], trg_text_vocab_transform, index=1,)
+    
+    datasets = []
+    
+    for key in split_:
+        src_text_transform = sequential_transforms(src_text_vocab_transform, 
+                                                   vocab_func(trg_vocab), 
+                                                   totensor(dtype=torch.long) )
+        trg_text_transform = sequential_transforms(trg_text_vocab_transform, 
+                                                   vocab_func(trg_vocab), 
+                                                   totensor(dtype=torch.long) )
+        
+        
+        datasets.append(TranslationDataset(data[key], (src_vocab, trg_vocab), (src_text_transform, trg_text_transform)))
+        
+    return _wrap_datasets(tuple(datasets), split_)
+
+
 if __name__ == '__main__':
     dm = MyDataModule()
     dm.prepare_data()
